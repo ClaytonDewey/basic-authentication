e32291aa3d7fae640a4ecd15a5d9755dc78c676b
ClaytonDewey <clay@dryadmedia.com>
2023-01-26 10:26:38 -0600

    Update to newer authenticate

--- src/Profile.js
+++ src/Profile.js
@@ -1,31 +1,21 @@
-import React, { useState, useEffect } from 'react';
-import { Auth } from 'aws-amplify';
-import { withAuthenticator, AmplifySignOut } from '@aws-amplify/ui-react';
+import React from 'react';
+import { Authenticator } from '@aws-amplify/ui-react';
 import Container from './Container';
 
 const Profile = () => {
-  useEffect(() => {
-    checkUser();
-  }, []);
-  const [user, setUser] = useState({});
-  const checkUser = async () => {
-    try {
-      const data = await Auth.currentUserPoolUser();
-      const userInfo = { username: data.username, ...data.attributes };
-      setUser(userInfo);
-    } catch (err) {
-      console.error('error: ', err);
-    }
-  };
   return (
-    <Container>
-      <h1>Profile</h1>
-      <h2>{user.username}</h2>
-      <h3>Email: {user.email}</h3>
-      <h4>Phone: {user.phone_number}</h4>
-      <AmplifySignOut />
-    </Container>
+    <Authenticator>
+      {({ signOut, user }) => (
+        <Container>
+          <h1>Profile</h1>
+          <h2>{user.username}</h2>
+          <h3>Email: {user.email}</h3>
+          <h4>Phone: {user.phone_number}</h4>
+          <button onClick={signOut}>Sign Out</button>
+        </Container>
+      )}
+    </Authenticator>
   );
 };
 
-export default withAuthenticator(Profile);
+export default Profile;
